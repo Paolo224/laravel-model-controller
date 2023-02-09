d837e6de67d4fcd7e65003083df957eea4c2682b
Paolo224 <picariellopaolo224@gmail.com>
2023-02-09 18:03:57 +0100

    Colegamento del db

--- app/Http/Controllers/Guest/MovieController.php
+++ app/Http/Controllers/Guest/MovieController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
     public function index()
     {
+        $movies = Movie::all();
+        //dd($movies);
         return view('home');
     }
 }
